25d9d46c0740f3cc707f96338cb6f1ebda854c33
vikas <vikasyetintala07@gmail.com>
2023-09-07 07:29:52 +0530

    all components implemented

---
+++ src/components/model_trainer.py
@@ -0,0 +1,89 @@
+import os 
+import sys 
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostRegressor,GradientBoostingRegressor
+from sklearn.metrics import r2_score 
+from catboost import CatBoostRegressor 
+from xgboost import XGBRegressor
+from dataclasses import dataclass
+from src.utils import evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join('artifacts','model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_mode_trainer(self,train_array,test_array):
+        try:
+            x_train,y_train,x_test,y_test=(train_array[:,:-1],train_array[:,-1],test_array[:,:-1],test_array[:,-1])
+            models={
+                'randomforstregressor':RandomForestRegressor(),
+                'decisiontreeregressor':DecisionTreeRegressor(),
+                'adaboostregressor':AdaBoostRegressor(),
+                'gradientboostingregressor':GradientBoostingRegressor(),
+                'xgboostregressor':XGBRegressor(),
+                'catboostregressor':CatBoostRegressor(verbose=False),
+                'linearregressor':LinearRegression()
+            }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
+                                             models=models,param=params)
+            best_model_score=max(sorted(model_report.values()))
+            best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            best_model=models[best_model_name]
+            if best_model_score<0.6:
+                raise CustomException("no best model found")
+            logging.info(f"best model found in both training and testing dataset")
+            save_object(self.model_trainer_config.trained_model_file_path,best_model)
+            predicted=best_model.predict(x_test)
+            r2_score=r2_score(y_test,predicted)
+            return r2_score
+
+            
+        except Exception as e:
+            raise CustomException(e,sys)
+            
